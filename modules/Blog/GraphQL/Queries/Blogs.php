028f97c5387371dd8b5575231d292b82b33e7c26
Meyz028 <makarshmit@gmail.com>
2025-08-26 10:12:39 +0300

    New

--- modules/Blog/GraphQL/Queries/Blogs.php
+++ modules/Blog/GraphQL/Queries/Blogs.php
@@ -15,13 +15,24 @@ class Blogs
     public function __invoke(null $_, array $args)
     {
         try {
+            $page = $args['page'] ?? 1;
+            $limit = $args['limit'] ?? 10;
+
             $blogs = BlogModel::query()
-                 ->where('active', true)
-                 ->latest('created_at')
-                 ->get();
+                ->where('active', true)
+                ->latest('created_at')
+                ->paginate($limit, ['*'], 'page', $page);
 
             return [
-                'data' => $blogs,
+                'data' => $blogs->items(),
+                'paginatorInfo' => [
+                    'currentPage' => $blogs->currentPage(),
+                    'lastPage' => $blogs->lastPage(),
+                    'total' => $blogs->total(),
+                    'firstItem' => $blogs->firstItem(),
+                    'lastItem' => $blogs->lastItem(),
+                ],
+
             ];
         } catch (Error $error) {
             throw new Error($error->getMessage());
